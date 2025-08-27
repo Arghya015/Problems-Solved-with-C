e993aac2e74e51c41c068fc38aa8fce3c4e08816
Arghya Roy <bubun15072006@gmail.com>
2025-08-27 19:10:20 +0530

    C to F.c

---
+++ C to F.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	float cel,fer;
+	printf("Give tremp in C=");
+	scanf("%f",&cel);
+	fer=(9*cel+160)/5;
+	printf("The converted tremp in ferhenhite=%f",fer);
+	return 0;
+	}
+	
